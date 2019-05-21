ee3b3776ec9193b146fe57de3f0bd2996060ef32
tanishkasinghal <tanishka23062gmail.com>
2019-05-21 23:57:25 +0530

    udp socket

--- UDP/receive.py
+++ UDP/receive.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
+# import lib
 import socket
-
+#target ip and port
 ip="127.0.0.1"
 port=2222
-
+#create a udp socket
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
+#bind ip and port
 s.bind((ip,port))
 while True:
 	msg=s.recvfrom(100)
--- UDP/send.py
+++ UDP/send.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
+#import lib
 import socket
-
+#target ip and port (use 0.0.0.0 for any connected ip--ec2instance)
 ip="127.0.0.1"
 port=2222
-
+#create a udp socket
+#              IP4(TCP--INET6)  udp(TCP--STREAM)
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 
